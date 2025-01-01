403b5a7673c02ff30bd8a6af6376dd44ada1744d
juanadls <eleo.dev09@gmail.com>
2025-01-01 14:49:58 -0300

    added viewmodel to handle state for view

--- app/src/main/java/com/example/mealsapp/ApiService.kt
+++ app/src/main/java/com/example/mealsapp/ApiService.kt
@@ -5,7 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 
-private val retrofit = Retrofit.Builder().baseUrl("www.themealdb.com/api/json/v1/1/").addConverterFactory(GsonConverterFactory.create()).build()
+private val retrofit = Retrofit.Builder().baseUrl("www.themealdb.com/api/json/v1/1/")
+    .addConverterFactory(GsonConverterFactory.create()).build()
 
 val recipeService = retrofit.create(ApiService::class.java)
 
