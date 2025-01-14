1b407e13a5ddbab686660ff9a109cec8089d3786
juanadls <eleo.dev09@gmail.com>
2025-01-14 06:53:00 -0300

    added categorydetailscree composable function

--- app/src/main/java/com/example/mealsapp/RecipeScreen.kt
+++ app/src/main/java/com/example/mealsapp/RecipeScreen.kt
@@ -1,8 +1,11 @@
 package com.example.mealsapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -10,15 +13,18 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 
 
 @Composable
-fun RecipeScreen() {
+fun RecipeScreen(navigateToDetail: (Category) -> Unit) {
     val recipeViewModel: MainViewModel = viewModel()
     val viewState by recipeViewModel.categoriesState
     Box {
@@ -32,24 +38,34 @@ fun RecipeScreen() {
             }
 
             else -> {
-                CategoryScreen(categories = viewState.list)
+                CategoryScreen(categories = viewState.list, navigateToDetail)
             }
         }
     }
 }
 
 @Composable
-fun CategoryScreen(categories: List<Category>) {
+fun CategoryScreen(
+    categories: List<Category>,
+    navigateToDetail: (Category) -> Unit
+) {
     LazyVerticalGrid(GridCells.Fixed(2)) {
         items(categories) { category ->
-            CategoryItem(category = category)
+            CategoryItem(category = category, navigateToDetail )
         }
     }
 }
 
 @Composable
-fun CategoryItem(category: Category) {
-    Column {
+fun CategoryItem(category: Category, navigateToDetail: (Category) -> Unit) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize()
+            .clickable { navigateToDetail( category) },
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
         Image(
             painter = rememberAsyncImagePainter(category.strCategoryThumb),
             contentDescription = null,
